6bc766e415f6a0d697dcd6eef32a25c56422e0b2
Hounkpe Metogbe Rufin <rhounkpe@gmail.com>
2021-05-18 20:31:48 +0200

    éInitial state

---
+++ src/main/java/be/digitcom/log/management/microservice/LogManagementMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package be.digitcom.log.management.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogManagementMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogManagementMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/digitcom/log/management/microservice/LogManagementMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.digitcom.log.management.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogManagementMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
